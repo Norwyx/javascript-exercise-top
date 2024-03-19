01ff277b9432bcec9f45af0e341cfbe12fb9df34
Manuel Salazar <mannisuso@gmail.com>
2024-03-19 08:46:41 -0500

    Finish tempConversion problem set

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  let convertedC = round((fahrenheit - 32) * (5/9), 1);
+  return convertedC;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  let convertedF = round((celsius * 9/5) + 32, 1)
+  return convertedF;
 };
+
+function round(value, precision) {
+  var multiplier = Math.pow(10, precision || 0);
+  return Math.round(value * multiplier) / multiplier;
+}
 
 // Do not edit below this line
 module.exports = {
