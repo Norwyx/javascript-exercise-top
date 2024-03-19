535c53038566b948bb180deec371a58a4a32f9fa
Manuel Salazar <mannisuso@gmail.com>
2024-03-19 08:14:32 -0500

    Finish sumAll problem set

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
-
+const sumAll = function(rangeBeg, rangeEnd) {
+    if (!Number.isInteger(rangeBeg) || !Number.isInteger(rangeEnd)) return 'ERROR';
+    if (rangeBeg < 0 || rangeEnd < 0) return 'ERROR';
+    if (rangeBeg > rangeEnd) {
+        let temp = rangeBeg;
+        rangeBeg = rangeEnd;
+        rangeEnd = temp;
+    }
+    let count = 0;
+    for (let i = rangeBeg; i <= rangeEnd; i++) {
+        count += i;
+    }
+    return count;
 };
 
 // Do not edit below this line
