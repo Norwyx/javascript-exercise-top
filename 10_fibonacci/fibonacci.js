94e590b9107718ee48985f52d25673c73fc4117a
Manuel Salazar <mannisuso@gmail.com>
2024-03-25 11:56:57 -0500

    Solve fibonacci problem set

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function(countArg) {
+    let count
+    if (typeof countArg != 'number') {
+        count = parseInt(countArg)
+    }
 
+    if (count == 0) return 0;
+    if (count < 0) return ('OOPS')
+
+    let defaultOne = 1
+    let defaultTwo = 0
+
+    for (let i = 2; i <= count; i--) {
+        let current = defaultOne + defaultTwo
+        defaultTwo = defaultOne
+        defaultOne = current
+    }
 };
 
 // Do not edit below this line
